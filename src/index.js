49bc8093ada3d834218e5b60167d562d26aba5f2
Joshua Zeltman <joshuazeltman@Joshuas-MacBook-Pro.local>
2023-04-12 09:18:28 -0400

    manually added CORS library

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "@adobe/universal-editor-cors";
+//import "@adobe/universal-editor-cors";
 import App from "./App";
 import "./index.css";
 
